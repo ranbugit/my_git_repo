987f5a1d031f05e23a6decc6e3ed0001e104442a
brian m. carlson <sandals@crustytoothpaste.net>
2025-05-22 18:55:21 +0000

    reflog-walk: expose read_complete_reflog
    In a future commit, we'll use this function and the corresponding free
    function to read the entire reflog.  Expose it in the header so we can
    do so.
    
    Include the appropriate header files so that our header is complete.
    
    Signed-off-by: brian m. carlson <sandals@crustytoothpaste.net>
    Signed-off-by: Junio C Hamano <gitster@pobox.com>

--- reflog-walk.h
+++ reflog-walk.h
@@ -1,9 +1,24 @@
 #ifndef REFLOG_WALK_H
 #define REFLOG_WALK_H
 
+#include "git-compat-util.h"
+#include "hash.h"
+
 struct commit;
 struct reflog_walk_info;
 struct date_mode;
+struct complete_reflogs {
+	char *ref;
+	char *short_ref;
+	struct reflog_info {
+		struct object_id ooid, noid;
+		char *email;
+		timestamp_t timestamp;
+		int tz;
+		char *message;
+	} *items;
+	int nr, alloc;
+};
 
 void init_reflog_walk(struct reflog_walk_info **info);
 void reflog_walk_info_release(struct reflog_walk_info *info);
@@ -23,5 +38,8 @@ void get_reflog_selector(struct strbuf *sb,
 int reflog_walk_empty(struct reflog_walk_info *walk);
 
 struct commit *next_reflog_entry(struct reflog_walk_info *reflog_info);
+
+void free_complete_reflog(struct complete_reflogs *array);
+struct complete_reflogs *read_complete_reflog(const char *ref);
 
 #endif
